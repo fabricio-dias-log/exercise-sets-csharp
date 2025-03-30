88c45efdd1fc7b1430f6c13ca8cb29af5bee6366
Fabricio Dias <fabricio270909@gmail.com>
2025-03-30 20:23:07 -0300

    feat: Get total users by file

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿namespace ExerciseSetsCsharp;
+﻿using ExerciseSetsCsharp.Entities;
+
+namespace ExerciseSetsCsharp;
 public class Program {
-    static void Main(){
+    static void Main() {
+        HashSet<LogRecord> set = new HashSet<LogRecord>();
+        
         Console.Write("Enter file full path: ");
         string? path = Console.ReadLine();
 
         try {
             using (StreamReader sr = File.OpenText(path)) {
                 while (!sr.EndOfStream) {
-                    string? line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    string[] line = sr.ReadLine().Split(' ');
+                    string name = line[0];
+                    DateTime instant = DateTime.Parse(line[1]);
+                    set.Add(new LogRecord { Username = name, Instant = instant });
                 }
+
+                Console.WriteLine($"Total users: {set.Count}");
             }
         }
         catch (IOException e) {
